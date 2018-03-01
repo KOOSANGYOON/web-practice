ff9bb6e7ec4887464cc64aabb1be129ec5c00902
KOOSANGYOON <nevergiveup936@naver.com>
2018-03-01 16:20:09 +0900

    update

---
+++ src/test/java/net/slipp/WebPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package net.slipp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+//asdf
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
